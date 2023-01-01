8a691ec92abc82e5802de06108890bdd0b492eb8
JX Hau <jxhau2001@gmail.com>
2023-01-02 01:12:45 +0800

    changed link

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ function App() {
         <Box px='10' pt='8'>
           <iframe style={{borderRadius:'12px'}} src="https://open.spotify.com/embed/track/06A0EIPtU2RYpNX8E1r4hO?utm_source=generator" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
         </Box>
-        
+
         <Box px='10' pt='8'>
-          <iframe style={{borderRadius:'12px'}} src="https://open.spotify.com/embed/playlist/2nNfnGIn1pyU1FG38obxEQ?utm_source=generator" width="100%" height="380" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+          <iframe style={{borderRadius:'12px'}} src="https://open.spotify.com/embed/playlist/37i9dQZF1F0sijgNaJdgit?utm_source=generator" width="100%" height="380" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
         </Box>
         <Box px='10' pt='8'>
           <iframe style={{borderRadius:'12px'}} src="https://open.spotify.com/embed/track/451TAZLUtPNsnvrK6KaKZB?utm_source=generator" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
