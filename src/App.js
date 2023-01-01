a555c404c26f78ff1a46eec8efebeb02d244bcf2
JX Hau <jxhau2001@gmail.com>
2023-01-02 01:03:27 +0800

    first comm

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { Container, Box } from '@chakra-ui/react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxW='100%'  bgColor='gray.800'>
+        <Box px='10' pt='8'>
+          <iframe style={{borderRadius:'12px'}} src="https://open.spotify.com/embed/track/06A0EIPtU2RYpNX8E1r4hO?utm_source=generator" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+        </Box>
+        
+        <Box px='10' pt='8'>
+          <iframe style={{borderRadius:'12px'}} src="https://open.spotify.com/embed/playlist/2nNfnGIn1pyU1FG38obxEQ?utm_source=generator" width="100%" height="380" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+        </Box>
+        <Box px='10' pt='8'>
+          <iframe style={{borderRadius:'12px'}} src="https://open.spotify.com/embed/track/451TAZLUtPNsnvrK6KaKZB?utm_source=generator" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+        </Box>
+
+        <Box px='10' pt='8'>
+          <iframe style={{borderRadius:'12px'}} src="https://open.spotify.com/embed/playlist/37i9dQZF1F0sijgNaJdgit?utm_source=generator" width="100%" height="380" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+        </Box>
+        
+        <Box px='10' py='8'>
+          <iframe style={{borderRadius:'12px'}} src="https://open.spotify.com/embed/track/1qbJx4kFTi7KbYIuJTE4CW?utm_source=generator" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+        </Box>
+    </Container>
   );
 }
 
